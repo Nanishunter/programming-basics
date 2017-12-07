93843df67a08ec31945b2175a3e758c8eb30adb2
1701542-Saimia <anttiyleinen@hotmail.com>
2017-12-07 12:56:19 +0200

    Made a volcabular calculator.

---
+++ Vocal-Calculator/Vokaalilaskuri/Vokaalilaskuri/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Vokaalilaskuri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma laskee vokaalien määrän, syötä sana tai lause.");
+            string userinput;
+            userinput = Console.ReadLine();
+            string syote = userinput.ToUpper();
+            int vokaali = 0;
+            foreach (char x in syote) 
+            {
+                if (x == 'A' || x == 'E' || x == 'I' || x == 'O' || x == 'U' || x == 'Y' || x == 'Ä' || x == 'Ö')
+                {
+                    vokaali = vokaali + 1;
+                }
+            }
+            Console.WriteLine($"Sanassa {userinput} on {vokaali} vokaalia");
+            Console.ReadLine();
+        }
+    }
+}
