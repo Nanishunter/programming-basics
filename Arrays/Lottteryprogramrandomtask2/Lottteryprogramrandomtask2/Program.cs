e371f04cf415013f197f51cc64baf18a980e8cfc
1701542-Saimia <anttiyleinen@hotmail.com>
2017-12-17 19:20:17 +0200

    Arrays done, task 3 yet to be working properly

---
+++ Arrays/Lottteryprogramrandomtask2/Lottteryprogramrandomtask2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lottteryprogramrandomtask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] numbers = new int[7];
+
+            for (int i = 0; i <= 6; i++)
+            {
+                numbers[i] = rnd.Next(1, 41);
+            }
+            Console.WriteLine("Lottorivi on: ");
+
+            Array.Sort(numbers);
+
+            foreach (int number in numbers)
+            {
+                Console.Write(number);
+                Console.Write(' ');
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Arrays/Taulukkotask1/Taulukkotask1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Taulukkotask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] iT = new int[100];
+            double sum = 0;
+            double average;
+
+            Console.WriteLine("100 arvottua lukua: ");
+
+            for (int i = 0; i < iT.Length; i++)
+            {
+                iT[i] = rnd.Next(0, 51);
+                Console.WriteLine($"{ i + 1 }. { iT[i] }");
+                sum = (double)sum + iT[i];
+            }
+            
+            average = sum / iT.Length;
+
+            Console.WriteLine($"Arvottujen lukujen keskiarvo: { average }.");
+            Console.ReadKey();
+        }
+    }
+}
